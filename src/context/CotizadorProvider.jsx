46cdff29dbcf0d787bdb884911a3fca066ac9673
guilleperalta <guille.peralta.dev@gmail.com>
2023-10-02 19:04:46 -0300

    Proyecto listo

---
+++ src/context/CotizadorProvider.jsx
@@ -0,0 +1,61 @@
+import { createContext, useState } from "react"
+import { calcularMarca , calcularPlan, formatearDinero, obtenerDiferenciaYear } from "../helpers"
+
+const CotizadorContext = createContext()
+
+const CotizadorProvider = ({children}) => {
+
+    const [datos , setDatos] = useState({
+        marca: '',
+        year: '',
+        plan:''
+    })
+
+    const [error , setError] = useState('')
+    const [resultado , setResultado] = useState(0)
+    const [cargando , setCargando] = useState(false)
+
+    const handleChangeDatos = e => {
+        setDatos({
+            ...datos,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const cotizarSeguro = () => {
+        let resultado = 2000
+        const diferencia = obtenerDiferenciaYear(+datos.year)
+        resultado -= ((diferencia * 0.03) * resultado )
+        resultado *= calcularMarca(datos.marca)
+        resultado *= calcularPlan(datos.plan)
+        resultado = formatearDinero(resultado)
+        setCargando(true)
+        setTimeout(() => {
+            setResultado(resultado)
+            setCargando(false)
+        }, 3000);
+    }
+
+
+    return (
+        <CotizadorContext.Provider
+            value={{
+                datos,
+                handleChangeDatos,
+                error,
+                setError,
+                cotizarSeguro,
+                resultado,
+                cargando
+            }}
+        >
+            {children}
+        </CotizadorContext.Provider>
+    )
+}
+
+export {
+    CotizadorProvider
+}
+
+export default CotizadorContext
